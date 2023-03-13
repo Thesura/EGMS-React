27da7215a3a1c3e386d7c14cf27c79ae7bf9ad70
Thesura <thesuracom@gmail.com>
2023-03-13 12:06:31 +0530

    march progress

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -2,69 +2,73 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FetchRequest from "../FetchRequest";
 
-function Schedule({staff}){
+function Schedule({ staff }) {
+  const [id, setID] = useState("");
+  const [area, setArea] = useState("");
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  const [reason, setReason] = useState("");
+  const [schedules, setSchedules] = useState();
+  const [tableBody, setTableBody] = useState("");
 
-    const [id, setID] = useState("");
-    const [area, setArea] = useState("");
-    const [start, setStart] = useState("");
-    const [end, setEnd] = useState("");
-    const [reason, setReason] = useState("");
-    const [schedules, setSchedules] = useState();
-    const[tableBody, setTableBody] = useState("");
+  useEffect(() => {
+    let url = "http://localhost:5000/schedule";
 
-    useEffect(() => {
-        let url = 'http://localhost:5000/schedule';
+    let response = FetchRequest(url, "GET");
 
-        let response = FetchRequest(url, 'GET');
+    response.then((value) => {
+      setSchedules(value.schedules);
+      console.log(value.schedules[0]);
+      // setID(value.schedules[0].id);
+      // setArea(value.schedules[0].area);
+      // setStart(value.schedules[0].start);
+      // setEnd(value.schedules[0].end);
+      // setReason(value.schedules[0].reason);
 
-        response.then((value) => {
-            // setSchedules(value.schedules);
-            console.log(value.schedules[0]);
-            setID(value.schedules[0].id);
-            setArea(value.schedules[0].area);
-            setStart(value.schedules[0].start);
-            setEnd(value.schedules[0].end);
-            setReason(value.schedules[0].reason);
+      // setTableBody(
+      // <tr>
+      // <td>{area}</td>
+      // <td>{start}</td>
+      // <td>{end}</td>
+      // <td>{reason}</td>
+      // </tr>
+      // )
+    });
+  }, []);
 
-// setTableBody(
-// <tr>
-// <td>{area}</td>
-// <td>{start}</td>
-// <td>{end}</td>
-// <td>{reason}</td>
-// </tr>
-// )
-        });
-
-
-
-    }, [schedules]);
-    
-    return(
-        <div>
-            <h1>Schedules</h1>
-            <table class="table table-bordered text-light">
-  <thead>
-    <tr>
-      <th scope="col">Area</th>
-      <th scope="col">Start</th>
-      <th scope="col">End</th>
-      <th scope="col">Reason</th>
-    </tr>
-  </thead>
-  <tbody>
-    {tableBody}
-  </tbody>
-</table>
-            <div style={staff ? { display: "Flex" } : { display: "none" }}>
-            <Link to="/createschedule" className="btn btn-outline-light btn-lg mt-auto">
-              Create Schedule
-            </Link>
-            </div>
-            
-        </div>
-    );
-
+  return (
+    <div>
+      <h1>Schedules</h1>
+      <table class="table table-bordered text-light">
+        <thead>
+          <tr>
+            <th scope="col">Area</th>
+            <th scope="col">Start</th>
+            <th scope="col">End</th>
+            <th scope="col">Reason</th>
+          </tr>
+        </thead>
+        <tbody>
+          {schedules && schedules.map((schedule) => {return(
+            <tr>
+              <td>{schedule.area}</td>
+              <td>{schedule.start.replace("T", " ")}</td>
+              <td>{schedule.end.replace("T", " ")}</td>
+              <td>{schedule.reason}</td>
+            </tr>
+          )})}
+        </tbody>
+      </table>
+      <div style={staff ? { display: "Flex" } : { display: "none" }}>
+        <Link
+          to="/createschedule"
+          className="btn btn-outline-light btn-lg mt-auto"
+        >
+          Create Schedule
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Schedule;
